7f77dd7b6a44bd1a972ce65c429004bcd2fea523
Andrés Ponce <75549856+aPonce2001@users.noreply.github.com>
2024-07-06 20:24:53 -0500

    build: actualiza favicon

--- src/questionnaire/questionnaire_ended/questionnaire_ended.gd
+++ src/questionnaire/questionnaire_ended/questionnaire_ended.gd
@@ -6,6 +6,7 @@ func _ready():
 	self.position.y = - get_viewport_rect().size.y
 
 func entry_animation_tween() -> Tween:
+	SfxPlayer.play_sfx("win.mp3", -8)
 	var tween = get_tree().create_tween().set_parallel(true)
 	tween.tween_property(self, "position:y", 0, 0.5).set_trans(Tween.TRANS_SINE)
 	return tween
